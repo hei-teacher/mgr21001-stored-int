ff238d62671ea542a60e5e0c176b7a6fdad3caf5
Lou Andria <lou@hei.school>
2025-07-08 09:22:40 +0300

    feat: GET /stored-int

---
+++ src/main/java/school/hei/storedint/endpoint/StoredIntController.java
@@ -0,0 +1,33 @@
+package school.hei.storedint.endpoint;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Random;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StoredIntController {
+
+  private static final Path FILE_PATH = Paths.get("stored-int.txt");
+
+  @GetMapping("/stored-int")
+  public String getStoredInt() throws InterruptedException, IOException {
+    Thread.sleep(10_000);
+    if (Files.exists(FILE_PATH)) {
+      String content = Files.readString(FILE_PATH).trim();
+      return "Stored value: " + content;
+    } else {
+      int randomInt = new Random().nextInt(1000); // [0,999]
+      Files.writeString(
+          FILE_PATH,
+          Integer.toString(randomInt),
+          StandardOpenOption.CREATE,
+          StandardOpenOption.WRITE);
+      return "Generated and stored: " + randomInt;
+    }
+  }
+}
